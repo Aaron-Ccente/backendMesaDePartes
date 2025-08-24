4fcdeda57a93eb2b48a8010bffa6837469ea2666
Aaron-Ccente <aronccente@gmail.com>
2025-08-24 18:12:22 -0500

    Para manejar errores de conexion a la bd

--- src/database/db.js
+++ src/database/db.js
@@ -2,10 +2,24 @@ import mysql2 from 'mysql2'
 import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } from '../config/config.js'
 
 const db = mysql2.createConnection({
-    host:DB_HOST,
-    user:DB_USER,
-    password:DB_PASSWORD,
-    database:DB_NAME,
-    port:DB_PORT
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    port: DB_PORT,
+    charset: 'utf8mb4',
+    supportBigNumbers: true,
+    bigNumberStrings: true,
 })
+
+// Manejar errores de conexión
+db.on('error', (err) => {
+    console.error('Error de conexión a la base de datos:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.log('Conexión a la base de datos perdida. Reintentando...');
+    } else {
+        throw err;
+    }
+});
+
 export default db
