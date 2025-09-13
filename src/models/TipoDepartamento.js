8ba972cf59061240227885acb49e34d00569c505
Aaron-Ccente <aronccente@gmail.com>
2025-09-12 23:34:41 -0500

    Modelos para los tipos de departamentos

--- src/models/TipoDepartamento.js
+++ src/models/TipoDepartamento.js
@@ -4,7 +4,7 @@ export class TipoDepartamento{
     static async findAll(){
     try {
       const [rows] = await db.promise().query(
-        'SELECT id_tipo_departamento, nombre_departamento, descripcion FROM tipo_departamento'
+        'SELECT id_tipo_departamento, nombre_departamento, descripcion FROM tipo_departamento ORDER BY id_tipo_departamento'
       );
       return rows;
     } catch (error) {
