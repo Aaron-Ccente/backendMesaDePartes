441f7228a59a75a2f70a4c6cb4a6a28f35a4781a
Aaron-Ccente <aronccente@gmail.com>
2025-09-30 22:52:49 -0500

    add: variables de entorno para la api de google drive

--- src/config/config.js
+++ src/config/config.js
@@ -8,3 +8,6 @@ export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DB_NAME = process.env.DB_NAME;
 export const DB_PORT = process.env.DB_PORT;
 export const JWT_SECRET = process.env.JWT_SECRET || 'mi_token_super_secreto_pnp';
+export const GOOGLE_CLIENT_EMAIL = process.env.GOOGLE_CLIENT_EMAIL;
+export const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY;
+export const GOOGLE_DRIVE_FOLDER_ID = process.env.GOOGLE_DRIVE_FOLDER_ID;
