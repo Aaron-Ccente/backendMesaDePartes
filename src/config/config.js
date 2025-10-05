8b5e4e2d4a847401f12eac9931d1f3d75600c75d
Aaron-Ccente <aronccente@gmail.com>
2025-10-04 20:00:14 -0500

    OAUTH cliente con acceso al drive

--- src/config/config.js
+++ src/config/config.js
@@ -11,3 +11,7 @@ export const JWT_SECRET = process.env.JWT_SECRET || 'mi_token_super_secreto_pnp'
 export const GOOGLE_CLIENT_EMAIL = process.env.GOOGLE_CLIENT_EMAIL;
 export const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY;
 export const GOOGLE_DRIVE_FOLDER_ID = process.env.GOOGLE_DRIVE_FOLDER_ID;
+export const GOOGLE_OAUTH_CLIENT_ID = process.env.GOOGLE_OAUTH_CLIENT_ID;
+export const GOOGLE_OAUTH_CLIENT_SECRET = process.env.GOOGLE_OAUTH_CLIENT_SECRET;
+export const GOOGLE_OAUTH_REDIRECT_URI = process.env.GOOGLE_OAUTH_REDIRECT_URI;
+export const GOOGLE_OAUTH_REFRESH_TOKEN = process.env.GOOGLE_OAUTH_REFRESH_TOKEN;
