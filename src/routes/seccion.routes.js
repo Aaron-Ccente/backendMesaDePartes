daffe378abc36c52acdf85b57a2025bf2305b669
Aaron-Ccente <aronccente@gmail.com>
2025-10-08 20:32:14 -0500

    Proteccion de rutas para obtener las secciones - solo usuarios autentificados

--- src/routes/seccion.routes.js
+++ src/routes/seccion.routes.js
@@ -1,8 +1,11 @@
 import {Router} from 'express';
 import { SectionController } from '../controllers/seccion.controller.js';
+import { authenticateToken } from '../middleware/authMiddleware.js';
 
 const router = Router();
 
+// Se requiere el token de usuarios autentificados (mesa de partes, perito y admin)
+router.use(authenticateToken);
 router.get("/", SectionController.getSectionById)
 
 export default router
