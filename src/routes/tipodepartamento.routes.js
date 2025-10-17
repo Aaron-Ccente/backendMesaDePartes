19293a339a379d9fb0ff7838fa15096afb19e4c5
Aaron-Ccente <aronccente@gmail.com>
2025-10-16 23:38:08 -0500

    rutas para los tipos de departamento

--- src/routes/tipodepartamento.routes.js
+++ src/routes/tipodepartamento.routes.js
@@ -1,13 +1,21 @@
 import { Router } from "express";
 import { TipodepartamentoController } from "../controllers/tipodepartamento.controller.js";
-import { authenticateToken } from "../middleware/authMiddleware.js";
+import { authenticateToken, requireAdmin } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
 // Se requiere el token de usuarios autentificados (mesa de partes, perito y admin)
 router.use(authenticateToken);
 
-// Get all tipos de departamento
-router.get('/', TipodepartamentoController.getAllTiposDepartamento)
+// Get all tipos de departamento (cualquiera autenticado)
+router.get("/", TipodepartamentoController.getAllTiposDepartamento);
+
+// Rutas que requieren ser admin
+router.use(requireAdmin);
+
+router.post("/", TipodepartamentoController.createTipoDepartamento);
+router.get("/:id", TipodepartamentoController.getTipoDepartamentoById);
+router.put("/:id", TipodepartamentoController.updateTipoDepartamento);
+router.delete("/:id", TipodepartamentoController.deleteTipoDepartamento);
 
 export default router;
